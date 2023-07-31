a2ea820e7f35422a793b4f6310d458a00ff22d2e
mkincaid63 <54153617+mkincaid63@users.noreply.github.com>
2023-07-31 13:30:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,16 @@
-// create web server
-// var http = require("http");
+// Create web server
+// Path: server.js
+// http://localhost:3000
+// http://localhost:3000/about
+// http://localhost:3000/contact
+// http://localhost:3000/contact?name=John&age=30
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2&calculateNumbers=3,4
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2&calculateNumbers=3,4&calculateNumbers=5,6
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2&calculateNumbers=3,4&calculateNumbers=5,6&calculateNumbers=7,8
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2&calculateNumbers=3,4&calculateNumbers=5,6&calculateNumbers=7,8&calculateNumbers=9,10
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2&calculateNumbers=3,4&calculateNumbers=5,6&calculateNumbers=7,8&calculateNumbers=9,10&calculateNumbers=11,12
+// http://localhost:3000/contact?name=John&age=30&address=123 Main St&skills=JavaScript,HTML,CSS&calculateNumbers=1,2&calculateNumbers=3,4&calculateNumbers=5,6&calculateNumbers=7,8&calculateNumbers=9
+
